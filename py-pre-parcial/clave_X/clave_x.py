0d90f1a061b3814f875c28d497424262f3af382d
LuisGonzalez32 <66042208+LuisGonzalez32@users.noreply.github.com>
2020-10-15 17:53:25 -0600

    pre parcial

---
+++ py-pre-parcial/clave_X/clave_x.py
@@ -0,0 +1,58 @@
+import math
+
+"""
+***************************************************************
+@@ ejercicio 1 @@
+un metodo python que haga la suma de 2 numeros
+2+4 = 6
+"""
+
+
+# start-->
+def suma():
+    total = 2 + 4
+    return total
+
+
+"""
+***************************************************************
+@@ ejercicio 2 @@
+un metodo python que haga la multiplicacion de 3 numeros
+2*4*5 = 40
+"""
+
+
+# start-->
+def multiplicacion():
+
+    total = 2*4*5
+    return total
+
+
+"""
+***************************************************************
+@@ ejercicio 3 @@
+un metodo python que haga la suma de los numeros de la lista
+numerosLista = [2,5,4,6,9,12]
+"""
+
+
+# start-->
+def sumarLista():
+    numerosLista = [2,5,4,6,9,12]
+    total = sum(numerosLista)
+    return total
+
+
+"""
+***************************************************************
+@@ ejercicio 4 @@
+colocar este proyecto en github
+colocar aca debajo la url
+enviar la url al correo balbino_a@hotmail.com
+"""
+
+
+# github url-->
+def getGithubUrl():
+    return "https://github.com/LuisGonzalez32/pre-parcial.git"
